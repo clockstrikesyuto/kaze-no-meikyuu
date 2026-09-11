5f10dedeb2280b9db1c12314ced609d9d25738ef
clockstrikesyuto <clock.strikes0204@gmail.com>
2026-09-11 17:17:01 +0900

    Refresh offline cache for Wind Maze v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kaze-no-meikyuu-v1';
-const ASSETS=['./','./index.html','./loader.js','./game.js.gz','./style.css.gz','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='kaze-no-meikyuu-v2';
+const ASSETS=['./','./index.html','./loader.js','./game.part.0','./game.part.1','./game.part.2','./game.part.3','./style.part.0','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})));});
